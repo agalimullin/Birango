e64879bc96767735e922fae049282ad62498e87a
agalimullin <amirzolotoy@gmail.com>
2017-03-27 12:57:51 +0300

    minor changes

--- Birango/app/controllers/mainController.js
+++ Birango/app/controllers/mainController.js
@@ -4,26 +4,33 @@ var birangoApp = angular.module('birangoApp', []);
 
 birangoApp.controller('birangoController', ['$scope', '$http', '$compile', function ($scope, $http, $compile) {
     angular.element(document).ready(function () {
-        var modal = $("#myModal");
-        var btn = $("#modalFavoriteBtn")[0];
-        var span = $(".close")[0];
+        var favoriteModal = $("#favoriteModal");
+        var countiesModal = $("#countriesModal");
 
-        // Открытие модалки по нажатию
-        btn.onclick = function () {
-            modal.show();
+        // Открытие модалки <избранное> по нажатию
+        $scope.openFavorite = function () {
+            favoriteModal.show();
         };
 
-        // Закрытие модалки по нажатию на X
-        span.onclick = function () {
-            modal.hide();
+        // Закрытие модалки <избранное> по нажатию на X
+        $scope.closeFavorite = function () {
+            favoriteModal.hide();
         };
 
         // Если модалка открыта и был совершен клик за её областью, закрываем
         window.onclick = function (event) {
-            console.log($(event.target)[0]);
-            if ($(event.target)[0] === modal[0]) {
-                modal.hide();
+            if ($(event.target)[0] === favoriteModal[0]) {
+                favoriteModal.hide();
             }
-        }
+        };
+
+        $scope.openCountries = function () {
+            countiesModal.show();
+            countiesModal.animate({'top':'0px'},1500);
+        };
+
+        $scope.closeCountries = function () {
+
+        };
     });
 }]);
